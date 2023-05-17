5421deea53e105e899f5f948f9a1982862dd87cf
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-05-17 08:48:59 +0000

    bookers2 copy

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: "home#top"
+  devise_for :users
+  get 'home/about' => 'home#about', as: "about"
+  
+  resources :books, only: [:index,:show,:edit,:create,:update,:destroy] 
+  resources :users, only: [:show,:edit,:index,:update]
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
